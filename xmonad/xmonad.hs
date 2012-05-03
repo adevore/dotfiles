b5281648c05539502a99b8106fb1447c36837dda
Aaron DeVore <aaron.devore@gmail.com>
2012-05-03 12:36:31 -0700

    Add wicd-gtk -t, add windowPromptGoto to select a window by name

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -5,16 +5,27 @@ import XMonad.Util.Run(spawnPipe, unsafeSpawn)
 import XMonad.Util.EZConfig(additionalKeysP)
 import XMonad.Actions.GridSelect
 import System.IO
+import XMonad.Prompt.Window(windowPromptGoto)
+import XMonad.Prompt(defaultXPConfig)
+import XMonad.Actions.SpawnOn
 
 -- Imports for TwinView
 import qualified Data.Map as M
 import qualified XMonad.StackSet as W
 import XMonad.Hooks.SetWMName
 
+-- startup :: X ()
+-- startup =
+--     spawn "kopete"
+--     --setWMName "LG3D"
+    
+
+
 main = do
   xmproc <- spawnPipe "xmobar"
   trayerproc <- unsafeSpawn "trayer --edge top --align right --SetDockType true --SetPartialStrut true --transparent true --expand true --width 7 --tint 0x191970 --height 12 &"
-  nmApplet <- unsafeSpawn "wicd-gtk"
+  kopete <- unsafeSpawn "if [[ `pgrep -u aaron kopete` == '' ]] then; kopete; fi"
+  nmApplet <- unsafeSpawn "wicd-gtk -t"
   pulseaudio <- unsafeSpawn "start-pulseaudio-kde"
   xmonad $ defaultConfig {
                manageHook = manageDocks <+> manageHook defaultConfig,
@@ -25,13 +36,15 @@ main = do
                    ppTitle = xmobarColor "green" "" .shorten 50
                },
                startupHook = setWMName "LG3D",
+               -- startupHook = startup,
                modMask = mod4Mask,
                terminal = "konsole"
                
              }
     `additionalKeysP`
     [
-      ("M-p", spawn "dmenu_run")
+      ("M-p", spawn "dmenu_run"),
+      ("M-l", windowPromptGoto defaultXPConfig)
     ]
 
 
